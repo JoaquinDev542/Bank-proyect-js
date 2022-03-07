2139ad405d4a7f6db68d75daebb4ad9b803f3c88
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-03-07 22:08:38 +0100

    Código agregado

--- main.js
+++ main.js
@@ -59,4 +59,58 @@
             // }
         // } 
 
-        
+
+// Código
+let userName = "";
+let password = "";
+let availableMoney = undefined;
+let depositMoney = "";
+let withdrawMoney = undefined;
+
+const functionConfirmCreateAccount = function () {
+    let confirmCreateAccount = confirm("¿Quieres crear una cuenta?");
+
+    if(confirmCreateAccount === true) {
+        console.log("Usted ya puede crearse una cuenta");
+        functionCreateAccount();
+        functionLogin();
+
+    } else {
+        console.log("Usted ha decidido no crearse una cuenta");
+    }
+}
+
+
+
+const functionCreateAccount = function () {
+    userName = prompt("Por favor, coloque su nombre de usuario");
+    password = prompt("Por favor, coloque una contraseña");
+    console.log("Usted se ha registrado correctamente");
+}
+
+const functionLogin = function () {
+    let loginMessage = confirm("Si desea iniciar sesión, confirmelo con la opción OK");
+
+    if(loginMessage === true) {
+        console.log("Usted puede iniciar sesión");
+        functionValidateAccount();
+    } else {
+        console.log("Usted ha decidido no iniciar sesión");
+    }
+} 
+
+const functionValidateAccount = function() {
+    let validateUser = prompt("Coloque su nombre de usuario");
+    let validatePassword = prompt("Coloque su contraseña");
+
+    if(validateUser === userName && validatePassword === password) {
+        console.log(`Hola ${userName}, usted ha iniciado sesión correctamente`);
+    } else {
+        console.warn("Su usuario y/o contraseña es incorrectos, por favor, intentelo de nuevo"); 
+
+    }
+
+}
+
+
+functionConfirmCreateAccount();
